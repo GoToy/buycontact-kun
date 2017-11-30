fa0b2ff53bdbee464491dfd900f596d2bc81c19c
TakatoiGo <takatoigo@TakatoiGos-MacBook-Pro.local>
2017-11-30 20:18:46 +0900

    oum

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /users
   # GET /users.json
   def index
-    client = User.client
-    body = request.body.read
-    events = client.parse_events_from(body)
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
-    }
-  end
+    end
 
   # GET /users/1
   # GET /users/1.json
@@ -44,7 +24,27 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    head :ok
+     client = User.client
+    body = request.body.read
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+  head :ok
 
 
   end
