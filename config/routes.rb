7f85f42ed1d223b9e226b8ab2c6cf4fa684d4a27
indigolain <indigo.lain@gmail.com>
2017-12-21 21:04:32 +0900

    add yes no uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      post 'update_contacts', to: 'contacts#update'
+      post 'show_contacts', to: 'contacts#show'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
