4b7b4acc962b7930c7493a61ea90a1bfe382ee12
TakatoiGo <takatoigo@TakatoiGos-MacBook-Pro.local>
2018-01-25 20:59:01 +0900

    make better ver6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,7 +62,7 @@ class UsersController < ApplicationController
           if parsed_message.present?
             before_update_num = user.remain || 0
             user.update(remain: before_update_num + parsed_message.to_i)
-           if parsed_message > 0
+           if parsed_message.to_i > 0
             if user.reload.remain == 7
               message = {
                 type: 'text',
@@ -78,12 +78,12 @@ class UsersController < ApplicationController
             if user.reload.remain == 7
               message = {
                 type: 'text',
-                text: "残数#{before_update_num}個に対し、#{parsed_message.abs.to_i}個引いて、#{user.reload.remain}個になりました\nhttps://www.lensmode.com/auth/login/redirectUrl/%252Fmypage%252Findex%252F/"
+                text: "残数#{before_update_num}個に対し、#{parsed_message.to_i.abs}個引いて、#{user.reload.remain}個になりました\nhttps://www.lensmode.com/auth/login/redirectUrl/%252Fmypage%252Findex%252F/"
               }
             else 
               message = {
                 type: 'text',
-                text: "残数#{before_update_num}個に対し、#{parsed_message.abs.to_i}個引いて、#{user.reload.remain}個になりました"
+                text: "残数#{before_update_num}個に対し、#{parsed_message.to_i.abs}個引いて、#{user.reload.remain}個になりました"
              } 
             end           
            end
